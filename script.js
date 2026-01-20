087f0434314dfc83851aa0144456fabac9d7d259
momo-gm <maude.mgm@gmail.com>
2026-01-20 16:09:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
 document.querySelectorAll(".box.main").forEach(box => {
   box.addEventListener("click", () => {
-    const sousListe = box.parentElement.querySelector("ul");
-    if (sousListe) {
-      sousListe.style.display =
-        sousListe.style.display === "none" ? "block" : "none";
-    }
-  });
-});
-
-document.querySelectorAll(".box.main").forEach(box => {
-  box.addEventListener("click", () => {
     const ul = box.parentElement.querySelector("ul");
     if (ul) {
-      ul.style.display = ul.style.display === "none" ? "block" : "none";
-    }
-  });
-});
-
-// Fonction clic pour dérouler / refermer
-document.querySelectorAll(".box.main").forEach(box => {
-  box.addEventListener("click", () => {
-    const ul = box.parentElement.querySelector("ul");
-    if (ul) {
-      // Récupère le style effectif pour bascule
+     
       const isHidden = window.getComputedStyle(ul).display === "none";
       ul.style.display = isHidden ? "block" : "none";
     }
   });
 });
+
