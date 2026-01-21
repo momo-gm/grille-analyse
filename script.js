26b8119b726aacc2bf1e67f0fd621608bcf367fe
momo-gm <maude.mgm@gmail.com>
2026-01-21 14:39:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.querySelectorAll(".box.main").forEach(box => {
   box.addEventListener("click", () => {
-    const ul = box.parentElement.querySelector("ul");
-    if (ul) {
-     
-      const isHidden = window.getComputedStyle(ul).display === "none";
-      ul.style.display = isHidden ? "block" : "none";
-    }
+    const li = box.parentElement;
+    const ul = li.querySelector(":scope > ul");
+
+    if (!ul) return;
+
+    const isHidden = window.getComputedStyle(ul).display === "none";
+    ul.style.display = isHidden ? "flex" : "none";
   });
 });
-
